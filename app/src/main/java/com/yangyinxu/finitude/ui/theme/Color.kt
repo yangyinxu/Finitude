f342da9d7af5f31493583abc215c43c61831a696
yangyinxu <yangyinxu@gmail.com>
2022-05-24 20:38:50 -0400

    Initial project setup

---
+++ app/src/main/java/com/yangyinxu/finitude/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.yangyinxu.finitude.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val DarkGray = Color(0xFF202020)
+val LightBlue = Color(0xFFD7E8DE)
+
+val RedOrange = Color(0xFFFFAB91)
+val RedPink = Color(0xFFF48FB1)
+val BabyBlue = Color(0xFF81DEEA)
+val Violet = Color(0xFFCF94DA)
+val LightGreen = Color(0xFFE7ED9B)
